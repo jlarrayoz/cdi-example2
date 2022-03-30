33cc8302f49ba19e179284cc72bb72112087c01e
jlarrayoz <juan.larrayoz@gmail.com>
2022-03-30 19:16:27 -0300

    Se cambia mensaje que mencionaba curso anterior

--- src/main/java/uy/org/cdi/example/Main.java
+++ src/main/java/uy/org/cdi/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		BookService service = new BookService();
-		Book book = service.createBook("Java EE7", 500f, "Curso JEE 7 BSE");
+		Book book = service.createBook("Java EE7", 500f, "Curso JEE 7");
 		System.out.println(book);
 
 		/*
